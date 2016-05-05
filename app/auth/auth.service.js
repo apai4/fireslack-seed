5b1940de30e53abf913c4878fd61c6039dd93a86
ramasubbaiya <some@gmail.com>
2016-05-04 23:03:05 -0400

    comment

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -1,7 +1,8 @@
 angular.module('angularfireSlackApp').factory('Auth',function($firebaseAuth, FirebaseUrl){
 	var ref = new Firebase(FirebaseUrl);
 	var auth = $firebaseAuth(ref);
-
+	//get the callback from firebase
 	return auth;	
 });
+
 
